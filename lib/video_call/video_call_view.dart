443b50cb2c3331308722048cc0a8666cda563795
BilalRehman08 <53112208+BilalRehman08@users.noreply.github.com>
2022-12-07 15:53:46 +0500

    refactor

--- lib/video_call/video_call_view.dart
+++ lib/video_call/video_call_view.dart
@@ -3,74 +3,72 @@ import 'package:agora_ui_kit/video_call/video_call_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class VideCallView extends StatelessWidget {
-  const VideCallView({super.key});
+class VideoCallView extends StatelessWidget {
+  const VideoCallView({super.key});
   @override
   Widget build(BuildContext context) {
-    VideoCallController controller = Get.put(VideoCallController());
-    return MaterialApp(
-      scaffoldMessengerKey: controller.scaffoldMessengerKey,
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Get started with Video Calling'),
+    VideoCallController controller = Get.isRegistered()
+        ? Get.find<VideoCallController>()
+        : Get.put(VideoCallController());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Get started with Video Calling'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        children: [
+          // Container for the local video
+          Container(
+            height: 240,
+            decoration: BoxDecoration(border: Border.all()),
+            child: Center(child: localPreview(controller: controller)),
           ),
-          body: ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-            children: [
-              // Container for the local video
-              Container(
-                height: 240,
-                decoration: BoxDecoration(border: Border.all()),
-                child: Center(child: localPreview(controller: controller)),
+          const SizedBox(height: 10),
+          //Container for the Remote video
+          Container(
+            height: 240,
+            decoration: BoxDecoration(border: Border.all()),
+            child: Center(child: remoteVideo(controller: controller)),
+          ),
+          // Button Row
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  onPressed:
+                      controller.isJoined ? null : () => {controller.join()},
+                  child: const Text("Join"),
+                ),
               ),
-              const SizedBox(height: 10),
-              //Container for the Remote video
-              Container(
-                height: 240,
-                decoration: BoxDecoration(border: Border.all()),
-                child: Center(child: remoteVideo(controller: controller)),
+              const SizedBox(width: 10),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed:
+                      controller.isJoined ? () => {controller.leave()} : null,
+                  child: const Text("Leave"),
+                ),
               ),
-              // Button Row
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: controller.isJoined
-                          ? null
-                          : () => {controller.join()},
-                      child: const Text("Join"),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: controller.isJoined
-                          ? () => {controller.leave()}
-                          : null,
-                      child: const Text("Leave"),
-                    ),
-                  ),
-                ],
-              ),
-              TextField(
-                controller: controller.channelTextController,
-                decoration: const InputDecoration(
-                    hintText: 'Type the channel name here'),
-              ),
-              TextField(
-                controller: controller.uid,
-                decoration:
-                    const InputDecoration(hintText: 'Type the uid name here'),
-              ),
-              TextField(
-                controller: controller.tokenRole,
-                decoration: const InputDecoration(
-                    hintText: 'Type the tokenrole name here'),
-              ),
-
-              // Button Row ends
             ],
-          )),
+          ),
+          TextField(
+            controller: controller.channelTextController,
+            decoration:
+                const InputDecoration(hintText: 'Type the channel name here'),
+          ),
+          TextField(
+            controller: controller.uid,
+            decoration:
+                const InputDecoration(hintText: 'Type the uid name here'),
+          ),
+          TextField(
+            controller: controller.tokenRole,
+            decoration:
+                const InputDecoration(hintText: 'Type the tokenrole name here'),
+          ),
+
+          // Button Row ends
+        ],
+      ),
     );
   }
 
