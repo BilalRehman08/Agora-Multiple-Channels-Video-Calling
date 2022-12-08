caf0f558bcee79b49668574847fd2a18af4f9835
BilalRehman08 <53112208+BilalRehman08@users.noreply.github.com>
2022-12-08 12:45:23 +0500

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:agora_ui_kit/video_call/video_call_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+// Import the generated file
+import 'firebase_options.dart';
 import 'video_call/video_call_controller.dart';
 
-void main() => runApp(const MaterialApp(home: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp(home: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
